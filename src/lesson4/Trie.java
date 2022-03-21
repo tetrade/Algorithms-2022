721570d81daa3a663aa30a396e825440314d6a57
Ivan Pyatlin <piatlin2002@gmail.com>
2022-03-21 23:01:04 +0300

    4 lesson

--- src/lesson4/Trie.java
+++ src/lesson4/Trie.java
@@ -2,8 +2,10 @@ package lesson4;
 
 import java.util.*;
 import kotlin.NotImplementedError;
+import lesson3.BinarySearchTree;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import java.util.Map.Entry;
 
 /**
  * Префиксное дерево для строк
@@ -82,6 +84,27 @@ public class Trie extends AbstractSet<String> implements Set<String> {
         return false;
     }
 
+    private boolean findNodeDel(String s) {
+        Node toDel = null;
+        char toDelChar = 0;
+        Node current = root;
+        for (char character : s.toCharArray()) {
+            if (current.children.size() > 1){
+                toDel = current;
+                toDelChar = character;
+            }
+            current = current.children.get(character);
+        }
+        if (toDel != null) {
+            toDel.children.remove(toDelChar);
+            return true;
+        } else {
+            return false;
+        }
+
+
+    }
+
     /**
      * Итератор для префиксного дерева
      *
@@ -91,9 +114,65 @@ public class Trie extends AbstractSet<String> implements Set<String> {
      */
     @NotNull
     @Override
-    public Iterator<String> iterator() {
-        // TODO
-        throw new NotImplementedError();
+    public Iterator<String> iterator() { return new TrieIterator(); }
+
+    public class TrieIterator implements Iterator<String> {
+        private final Deque<Iterator<Entry<Character, Node>>> deq = new ArrayDeque<>();
+        private final StringBuilder sb = new StringBuilder();
+        private String lastNext;
+
+
+
+        private TrieIterator() {
+            if (Trie.this.size != 0) { deq.push(root.children.entrySet().iterator()); }
+        }
+
+        private void getNext() {
+            Iterator<Entry<Character, Node>> iter = deq.peek();
+            while (iter != null) {
+                while (iter.hasNext()) {
+                    Entry<Character, Node> n = iter.next();
+                    if (n.getKey() == (char) 0) {
+                        lastNext = sb.toString();
+                        return;
+                    }
+                    sb.append(n.getKey());
+                    iter = n.getValue().children.entrySet().iterator();
+                    deq.push(iter);
+                }
+                deq.remove();
+                if (sb.length() != 0) sb.deleteCharAt(sb.length() - 1);
+                iter = deq.peek();
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            removeEmptyDeq();
+            return !deq.isEmpty();
+        }
+
+        @Override
+        public String next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            getNext();
+            return lastNext;
+        }
+
+        @Override
+        public void remove() {
+            if (lastNext == null) throw new IllegalStateException();
+            deq.peek().remove();
+            lastNext = null;
+            size--;
+        }
+
+        private void removeEmptyDeq() {
+            while (deq.peek() != null && !deq.peek().hasNext()) {
+                if (sb.length() != 0) sb.deleteCharAt(sb.length() - 1);
+                deq.remove();
+            }
+       }
     }
 
 }
